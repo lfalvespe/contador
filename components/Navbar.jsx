0e68ac0a54a9450d25a90d3bdd8904ff734598a8
Fernando Alves <lfalvespe@gmail.com>
2025-04-09 03:58:12 -0300

    Navbar css adjusted

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav>
-            <a href="">Contador Numérico</a>
+            <a href="https://contador-reactjs.vercel.app/">Contador Numérico</a>
             <span>|</span>
-            <a href="https://numeros-aleatorios-js.vercel.app/" target='_blank'>Gerador de números aleatórios</a>
+            <a href="https://numerosaleatorios-react.vercel.app/" target='_blank'>Gerador de números aleatórios</a>
         </nav>
     )
 }
