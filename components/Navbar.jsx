3e320eb1e8bf97beaab15a5eeba3618b2a819569
Fernando Alves <lfalvespe@gmail.com>
2025-04-11 16:56:54 -0300

    navbar flex adjusted to wrap

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,27 +4,23 @@ const Navbar = () => {
     return (
         <nav>
              <a
-                class="text-decoration-none text-capitalize rounded"
                 href=""
             > <span class="link-icon">🎰</span>Contador Numérico
             </a>
 
             <a
-                class="text-decoration-none text-capitalize rounded"
                 href="https://consultaporcep.vercel.app/"
                 target="_blank"
             > <span class="link-icon">📬</span>Busca Cep
             </a>
 
             <a
-                class="text-decoration-none text-capitalize rounded"
                 href="https://climagora.vercel.app/"
                 target="_blank"
             > <span class="link-icon">⛅</span>Clima Agora
             </a>
     
             <a
-                class="text-decoration-none text-capitalize rounded"
                 href="https://numerosaleatorios-react.vercel.app/"
                 target="_blank"
             > <span class="link-icon">🎲</span>Números aleatórios
