98fa169c78f42b913a71d1569e2b42e79c4fbd2d
Fernando Alves <lfalvespe@gmail.com>
2025-04-13 13:16:16 -0300

    instructions ul switched to divs

--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Instructions = () => {
     return (
         <p className="instructions">
-            <ul>
-                <li>Contagem máxima 99999</li>
-                <li>Após atingir o limte máximo o contador é reiniciado</li>
-                <li>Registro salvo no navegador.</li>
-            </ul>
+            <div className='list'>
+                <div className='line'>▫️ Contagem máxima 99999</div>
+                <div className='line'>▫️Após atingir o limte máximo o contador é reiniciado</div>
+                <div className='line'>▫️ Registro salvo no navegador.</div>
+            </div>
         </p>
     )
 }
